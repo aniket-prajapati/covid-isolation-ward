0cd08fb1035e141724eebebec07ea401762878a1
aniketp <aniket.prajapati@resolve.io>
2021-07-01 17:02:18 +0530

    Added messaging and enhanced if/else condition

--- covid_robot.py
+++ covid_robot.py
@@ -1,62 +1,46 @@
 class CovidBot:
+    # Initializing variables
+    """ This class will be responsible for processing covid robot input and return an output
+        with the final position of the robot """
 
     def __init__(self, coordinates, position, movement):
-        self.x_axis = None
-        self.y_axis = None
-        self.orientation = None
-        self.input_coordinates = coordinates
-        self.input_position = position
-        self.input_movement = movement
-        self.parse_input()
-
-    def rotate_left(self):
-        if self.orientation == "N":
-            self.orientation = "W"
-        elif self.orientation == "W":
-            self.orientation = "S"
-        elif self.orientation == "S":
-            self.orientation = "E"
-        elif self.orientation == "E":
-            self.orientation = "N"
-
-    def rotate_right(self):
-        if self.orientation == "N":
-            self.orientation = "E"
-        elif self.orientation == "E":
-            self.orientation = "S"
-        elif self.orientation == "S":
-            self.orientation = "W"
-        elif self.orientation == "W":
-            self.orientation = "N"
-
-    def move_position(self):
-        if self.orientation == "N":
-            self.y_axis += 1
-        elif self.orientation == "E":
-            self.x_axis += 1
-        elif self.orientation == "S":
-            self.y_axis -= 1
-        elif self.orientation == "W":
-            self.x_axis -= 1
-
-    def parse_input(self):
-        self.input_coordinates = self.input_coordinates.split(self.input_coordinates[1])
-        self.input_position = self.input_position.split(self.input_position[1])
+        self.input_coordinates = coordinates.split(coordinates[1])
+        self.input_position = position.split(position[1])
         self.x_axis = int(self.input_position[0])
         self.y_axis = int(self.input_position[1])
+        self.x_max = int(self.input_coordinates[0])
+        self.y_max = int(self.input_coordinates[1])
         self.orientation = self.input_position[2]
+        self.input_movement = movement
+        self.check_input()
+        self.left_orientation = {"N": "W", "W": "S", "S": "E", "E":"N"}
+        self.right_orientation = {"N": "E", "E": "S", "S": "W", "W":"N"}
 
+    def move_position(self):
+        """ Method to move position of the robot within the grid coordinates """
+        if self.orientation == "N" and self.y_max > self.y_axis:
+            self.y_axis += 1
+        elif self.orientation == "E" and self.x_max > self.x_axis:
+            self.x_axis += 1
+        elif self.orientation == "S" and self.y_axis > 0:
+            self.y_axis -= 1
+        elif self.orientation == "W" and self.x_axis > 0:
+            self.x_axis -= 1
+
+    def check_input(self):
+        """ Method to check if given inputs for the robot position are not out of the grid """
         if int(self.input_position[0]) > int(self.input_coordinates[0]) or \
                 int(self.input_position[1]) > int(self.input_coordinates[1]):
             print("Robot Position out of grid coordinates")
             exit()
     
     def process_input(self):
+        """ Method to compute the final position the robot will go to and return the output """
         for i in range(len(self.input_movement)):
             if self.input_movement[i] == 'L':
-                self.rotate_left()
+                self.orientation = self.left_orientation.get(self.orientation)
             elif self.input_movement[i] == 'R':
-                self.rotate_right()
+                self.orientation = self.right_orientation.get(self.orientation)
             else:
                 self.move_position()
 
